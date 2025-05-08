7728281c7a210077d49f4a745bebba33e8c54642
DiegoSintaxWizard <josediegotejemayo@gmail.com>
2025-05-07 23:00:18 -0400

    Add files via upload

---
+++ calculadora/calculadora.java
@@ -0,0 +1,15 @@
+ package calculadora;
+
+public class calculadora {
+
+    // declaran sus varibles conrrespondientes 
+    public Calculadora(){
+        // este es el metodo constructor
+    }
+
+    // de aca para adelante hacen sus metodos, osea las funciones matematicas que van a tener nuestra calculadora ejemplo
+
+    public double sumar (double valorA, double valorB){
+        return valorA + valorB;
+    }
+}
---
+++ calculadora/principal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class principal {
+    public static void main (String arg []){
+        Calculadora calcu = new Calculadora ();
+        // aca pedimos los valores que neceistamos ejemplo
+        Scanner sp = new Scanner (System.in);
+        System.out.println("ingresa el primer valor");
+         double valor1 = sp.nextDouble();
+         System.out.println("ingresa el segundo valor");
+         double valor2 = sp.nextDouble();
+         System.out.println("Suma = 1, Resta = 2 ...");
+         int opcion = sp.nextInt();
+    switch (opcion ) {
+        case 1:
+            calu.sumar(valor1, valor2);
+            break;
+    
+        default:
+            break;
+    }
+    }
+}
